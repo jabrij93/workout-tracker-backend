23fa9e32c1833f2e6775df62abcbb0b915815848
jabss <jabrijuhinin93@gmail.com>
2024-10-04 16:05:25 +0800

    simple web server is set-up correctly

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-let test1 = "Hello World !"
+const http = require('http')
 
-console.log(test1)
+let workoutData = [
+    {
+      "workouts": "pull-ups",
+      "likes": 5,
+      "id": "e05b",
+      "date": "2024-02-22"
+    },
+    {
+      "workouts": "dips",
+      "likes": 3,
+      "id": "dfb1",
+      "date": "2024-02-23"
+    },
+    {
+      "workouts": "push-up",
+      "likes": 2,
+      "id": "f11b",
+      "date": "2024-01-24"
+    },
+    {
+      "id": "3130",
+      "workouts": "test add workout",
+      "likes": 8,
+      "date": "2024-03-25"
+    },
+    {
+      "id": "4e15",
+      "workouts": "sit-ups",
+      "likes": 4
+    }
+  ]
+
+  const app = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify(workoutData))
+  })
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
