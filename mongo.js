10abeb02d9f212c195d32df06e5f77c2297330ab
jabss <jabrijuhinin93@gmail.com>
2024-10-21 10:36:21 +0400

    objects(new data) can now be stored dynamically(with date only, with likes only, with both)

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-if (process.argv.length < 5) {
+if (process.argv.length < 4) {
   console.log('Usage: node mongo.js <password> <workout> <detail> <likes> [date]');
   process.exit(1);
 }
@@ -9,8 +9,19 @@ if (process.argv.length < 5) {
 const password = process.argv[2];
 const workoutName = process.argv[3];
 const workoutDetail = process.argv[4];
-const workoutLikes = Number(process.argv[5]) ? Number(process.argv[5]) : 0  // Convert likes to a number
-const workoutDate = process.argv[6] ? process.argv[6] : new Date().toISOString().split('T')[0];  // If date is provided, use it, otherwise use today's date
+// Check if the 5th argument is a number (likes), otherwise treat it as a date or skip
+let workoutLikes;
+let workoutDate;
+
+if (process.argv[5] && !isNaN(process.argv[5])) {
+  // If the 5th argument is a number, it's likes, and we expect date to be in the 6th argument
+  workoutLikes = Number(process.argv[5]);
+  workoutDate = process.argv[6] ? process.argv[6] : new Date().toISOString().split('T')[0];
+} else {
+  // If the 5th argument is not a number, it's the date and likes should default to 0
+  workoutLikes = 0;
+  workoutDate = process.argv[5] ? process.argv[5] : new Date().toISOString().split('T')[0];
+}
 
 const url = process.env.MONGODB_URI
 
@@ -28,20 +39,20 @@ const workoutSchema = new mongoose.Schema({
 const Workout = mongoose.model('Workout', workoutSchema)
 
 const workout = new Workout({
-  workouts: 'Pull-ups',
-  date: '2024-05-05',
-  detail: "3 sets x 9 reps",
-  likes: 0
+  workouts: workoutName,
+  date: workoutDate,
+  detail: workoutDetail,
+  likes: workoutLikes
 })
 
-// workout.save().then(result => {
-//   console.log('workout saved!')
-//   mongoose.connection.close()
-// })
-
-Workout.find({}).then(result => {
-  result.forEach(workout => {
-    console.log(workout)
-  })
+workout.save().then(result => {
+  console.log('workout saved!')
   mongoose.connection.close()
 })
+
+// Workout.find({}).then(result => {
+//   result.forEach(workout => {
+//     console.log(workout)
+//   })
+//   mongoose.connection.close()
+// })
