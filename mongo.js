04abf4f4c1099574d8b6aee6dbd2bb5e1542786e
jabss <jabrijuhinin93@gmail.com>
2024-10-27 12:32:40 +0400

    update mongo.js file with eslint

--- mongo.js
+++ mongo.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const password = process.argv[2]
-const workoutName = process.argv[3]
-const workoutDetail = process.argv[4]
+const password = process.argv[2];
+const workoutName = process.argv[3];
+const workoutDetail = process.argv[4];
 // Check if the 5th argument is a number (likes), otherwise treat it as a date or skip
-let workoutLikes
-let workoutDate
+let workoutLikes;
+let workoutDate;
 
 const url = process.env.MONGODB_URI
 
@@ -27,27 +27,27 @@ const Workout = mongoose.model('Workout', workoutSchema)
 if (process.argv.length === 3) {
   Workout.find({})
     .then((result) => {
-      console.log('Workout:')
+      console.log("Workout:")
       result.forEach((workout) => {
-        console.log(`${workout.workouts} ${workout.detail} ${workout.date} ${workout.likes}`)
-      })
-      mongoose.connection.close()
+        console.log(`${workout.workouts} ${workout.detail} ${workout.date} ${workout.likes}`);
+      });
+      mongoose.connection.close();
     })
 } else if (process.argv.length >= 4) {
   // If workout details are provided, proceed to save a new workout entry
 
   // Check if the 5th argument is a number (likes), otherwise treat it as a date or skip
-  let workoutLikes
-  let workoutDate
+  let workoutLikes;
+  let workoutDate;
 
   if (process.argv[5] && !isNaN(process.argv[5])) {
     // If the 5th argument is a number, it's likes, and we expect date to be in the 6th argument
-    workoutLikes = Number(process.argv[5])
-    workoutDate = process.argv[6] ? process.argv[6] : new Date().toISOString().split('T')[0]
+    workoutLikes = Number(process.argv[5]);
+    workoutDate = process.argv[6] ? process.argv[6] : new Date().toISOString().split('T')[0];
   } else {
     // If the 5th argument is not a number, it's the date and likes should default to 0
-    workoutLikes = 0
-    workoutDate = process.argv[5] ? process.argv[5] : new Date().toISOString().split('T')[0]
+    workoutLikes = 0;
+    workoutDate = process.argv[5] ? process.argv[5] : new Date().toISOString().split('T')[0];
   }
 
   const workout = new Workout({
@@ -55,15 +55,15 @@ if (process.argv.length === 3) {
     date: workoutDate,
     detail: workoutDetail,
     likes: workoutLikes,
-  })
+  });
 
   workout
     .save()
     .then((result) => {
-      console.log('Workout saved!')
-      mongoose.connection.close()
+      console.log('Workout saved!');
+      mongoose.connection.close();
     })
 } else {
-  console.log('Usage: node mongo.js <password> <workout> <detail> [likes] [date]')
-  mongoose.connection.close()
+  console.log('Usage: node mongo.js <password> <workout> <detail> [likes] [date]');
+  mongoose.connection.close();
 }
