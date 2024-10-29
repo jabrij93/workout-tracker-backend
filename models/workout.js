5e9179f45422fbe93fc43a779f785d5d3b7f3cba
jabss <jabrijuhinin93@gmail.com>
2024-10-29 15:17:18 +0400

    uibuild

--- models/workout.js
+++ models/workout.js
@@ -10,8 +10,8 @@ const workoutSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function (value) {
-        // Regular expression for matching "dd-mm-yyyy" or "dd/mm/yyyy" or single-digit day/month
-        return /^(0?[1-9]|[12][0-9]|3[01])[-\/](0?[1-9]|1[0-2])[-\/]\d{4}$/.test(value)
+        // Regular expression for matching "dd-mm-yyyy" or "d-m-yyyy"
+        return /^(0?[1-9]|[12][0-9]|3[01])-(0?[1-9]|1[0-2])-\d{4}$/.test(value)
       },
       message: props => `${props.value} is not a valid date format. Use dd-mm-yyyy or d-m-yyyy.`,
     },
