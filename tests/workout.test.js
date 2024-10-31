e11b15a72411009ef89a07560c920da374038225
jabss <jabrijuhinin93@gmail.com>
2024-10-31 15:23:49 +0400

    total likes test working !

--- tests/workout.test.js
+++ tests/workout.test.js
@@ -2,9 +2,46 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+const generateId = () => {
+  // Generate two random lowercase letters (a-z)
+  const letter1 = String.fromCharCode(
+    Math.floor(Math.random() * 26) + 97 // First random letter
+  )
+
+  const letter2 = String.fromCharCode(
+    Math.floor(Math.random() * 26) + 97 // First random letter
+  )
+
+  // Generate two random digits (0-9)
+  const number1 = Math.floor(Math.random() * 9).toString()
+  const number2 = Math.floor(Math.random() * 9).toString()
+
+  // Combine the two letters and two digits
+  return letter1 + number1 + letter2 + number2
+}
+
 test('dummy returns one', () => {
   const workouts = []
 
   const result = listHelper.dummy(workouts)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  test('totallikes', () => {
+
+    const workouts = []
+
+    const newWorkout = {
+      id: generateId(),
+      workouts: 'pull-ups',
+      likes: 10,
+      date: '5/7/2024'
+    }
+
+    workouts.push(newWorkout)
+
+    const result = listHelper.totalLikes(workouts)
+    assert.strictEqual(result, 10)
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,10 @@ const dummy = (workouts) => {
   return 1
 }
 
+const totalLikes = (workouts) => {
+  return workouts.reduce((sum, workout) => sum + workout.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
