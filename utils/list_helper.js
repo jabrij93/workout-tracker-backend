a796bc19d931150e7cab288fe58903e88d5b2eaa
jabss <jabrijuhinin93@gmail.com>
2024-11-03 23:22:41 +0400

    add accountWithMostWorkout helper function

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,6 +10,10 @@ const mostLikedWorkout = (workouts) => {
   return workouts.reduce((max, workout) => workout.likes > max.likes ? workout : max, workouts[0])
 }
 
+const accountWithMostWorkout = (accounts) => {
+  return accounts.reduce((max, account) => account.workouts > max.workouts ? account : max, accounts[0])
+}
+
 module.exports = {
-  dummy, totalLikes, mostLikedWorkout
+  dummy, totalLikes, mostLikedWorkout, accountWithMostWorkout
 }
