59a9a83496fbcd91718a6210822a618c95a9d26f
jabss <jabrijuhinin93@gmail.com>
2024-11-05 00:21:52 +0400

    add 'assert' from node:assert

--- tests/workout_api.test.js
+++ tests/workout_api.test.js
@@ -1,4 +1,5 @@
 const { test, after } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -10,6 +11,19 @@ test('workouts are returned as json', async () => {
     .get('/api/workouts')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, 2)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(e => e.content)
+  assert.strictEqual(contents.includes('HTML is easy'), true)
 })
 
 after(async () => {
