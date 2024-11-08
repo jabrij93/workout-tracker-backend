a3906af55e3761e77eacaefc3054920fd93ca755
jabss <jabrijuhinin93@gmail.com>
2024-11-08 08:07:32 +0400

    refactor into helper function module

---
+++ tests/test_helper.js
@@ -0,0 +1,35 @@
+const Workout = require('../models/workout')
+
+const initialWorkouts = [
+  {
+    workouts: 'pull-ups',
+    likes: 10,
+    date: '5/7/2024',
+  },
+  {
+    workouts: 'bench press',
+    likes: 15,
+    date: '5/7/2024',
+  },
+  { workouts: 'dumbbell press',
+    likes: 20,
+    date: '5/7/2024'
+  }
+]
+
+const nonExistingId = async () => {
+  const workout = new Workout({ workouts: 'willremovethissoon' })
+  await workout.save()
+  await workout.deleteOne()
+
+  return workout._id.toString()
+}
+
+const workoutsInDb = async () => {
+  const workouts = await Workout.find({})
+  return workouts.map(workout => workout.toJSON())
+}
+
+module.exports = {
+  initialWorkouts, nonExistingId, workoutsInDb
+}
