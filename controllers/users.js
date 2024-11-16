a5e17c6a364beae47230c496e448bfc87e2da686
jabss <jabrijuhinin93@gmail.com>
2024-11-16 16:13:07 +0400

    add validation so password must be at least 4 characters long and if not return an error status 400

--- controllers/users.js
+++ controllers/users.js
@@ -10,6 +10,12 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  if (!password || password.length < 4) {
+    return response.status(400).json({
+      error: 'Password length must be at least 4 characters long',
+    })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
