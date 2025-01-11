ec4af79029670620f39ee8f3a1e56d42ca680120
jabss <jabrijuhinin93@gmail.com>
2025-01-11 21:27:03 +0400

    configured state of database when run in test mode

---
+++ controllers/testing.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const Workout = require('../models/workout')
+const User = require('../models/user')
+
+router.post('/reset', async (request, response) => {
+  await Workout.deleteMany({})
+  await User.deleteMany({})
+
+  response.status(204).end()
+})
+
+module.exports = router
