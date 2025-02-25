68c4cea18251c0db1d89a3481ed9b45ed8971b66
jabss <jabrijuhinin93@gmail.com>
2025-02-26 00:56:49 +0800

    commented generateId function

--- tests/workout.test.js
+++ tests/workout.test.js
@@ -2,23 +2,23 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-const generateId = () => {
-  // Generate two random lowercase letters (a-z)
-  const letter1 = String.fromCharCode(
-    Math.floor(Math.random() * 26) + 97 // First random letter
-  )
+// const generateId = () => {
+//   // Generate two random lowercase letters (a-z)
+//   const letter1 = String.fromCharCode(
+//     Math.floor(Math.random() * 26) + 97 // First random letter
+//   )
 
-  const letter2 = String.fromCharCode(
-    Math.floor(Math.random() * 26) + 97 // First random letter
-  )
+//   const letter2 = String.fromCharCode(
+//     Math.floor(Math.random() * 26) + 97 // First random letter
+//   )
 
-  // Generate two random digits (0-9)
-  const number1 = Math.floor(Math.random() * 9).toString()
-  const number2 = Math.floor(Math.random() * 9).toString()
+//   // Generate two random digits (0-9)
+//   const number1 = Math.floor(Math.random() * 9).toString()
+//   const number2 = Math.floor(Math.random() * 9).toString()
 
-  // Combine the two letters and two digits
-  return letter1 + number1 + letter2 + number2
-}
+//   // Combine the two letters and two digits
+//   return letter1 + number1 + letter2 + number2
+// }
 
 test('dummy returns one', () => {
   const workouts = []
